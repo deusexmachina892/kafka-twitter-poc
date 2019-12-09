5115c4c2dedcd7265bf0dc34bace91345f9ff4e6
deusexmachina892 <deusexmachina892@gmail.com>
2019-12-09 14:31:48 +0530

    Refactors

---
+++ src/main/java/com/deus/kafka/TwitterStream.java
@@ -0,0 +1,15 @@
+package com.deus.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TwitterStream
+ */
+@SpringBootApplication
+public class TwitterStream {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterStream.class, args);
+    }
+}
--- src/test/java/com/deus/kafka/AppTest.java
+++ src/test/java/com/deus/kafka/AppTest.java
@@ -1,20 +1,13 @@
 package com.deus.kafka;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import org.junit.Test;
+@SpringBootTest
+class DatastreamApplicationTests {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
